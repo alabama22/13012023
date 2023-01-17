44a83d5105c262553aaf5a9e9d2c720a80ba3927
Zarina <catwarning@gmail.com>
2023-01-17 14:21:41 +0600

    Added Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿int randomNumber= new Random().Next(100, 1000);
+Console.WriteLine ($"Конечное число=> {randomNumber}");//итнрополяция строки
+int finishNumber=GetNumber(randomNumber);
+Console.WriteLine($"Конечное число=> {finishNumber}");
+
+int GetNumber(int number)
+{
+    int firstNumber=number/100;
+    int secondNumber=number%10;
+    int result=firstNumber*10+secondNumber;
+    return result;
+}
